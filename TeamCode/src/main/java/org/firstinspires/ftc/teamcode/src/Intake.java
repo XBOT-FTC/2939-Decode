6bdff4aa3121513f13eef56ae29c899aa609aacf
ThanhLamTr <lamt16482@gmail.com>
2025-11-17 17:47:11 -0800

    copyof3231

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/src/Intake.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/src/Intake.java
@@ -1,4 +1,32 @@
 package org.firstinspires.ftc.teamcode.src;
 
+import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
+import com.qualcomm.robotcore.hardware.DcMotor;
+import com.qualcomm.robotcore.hardware.DcMotorSimple;
+import com.qualcomm.robotcore.hardware.HardwareMap;
+import com.qualcomm.robotcore.hardware.Gamepad;
+import org.firstinspires.ftc.robotcore.external.Telemetry;
+
+
 public class Intake {
+    private final DcMotor intakeMotor;
+
+    public Intake(HardwareMap hardwareMap) {
+        this.intakeMotor = hardwareMap.dcMotor.get("intakeMotor");
+        this.intakeMotor.setDirection(DcMotorSimple.Direction.FORWARD);
+    }
+
+    public void run(Gamepad gamepad) {
+        if (gamepad.right_trigger >= 0.5) { // Intake
+            intakeMotor.setPower(1.0);
+        } else if (gamepad.left_trigger >= 0.5) { // Outtake
+            intakeMotor.setPower(-1.0);
+        } else {
+            intakeMotor.setPower(0);
+        }
+    }
+
+    public double getIntakePower() {
+        return intakeMotor.getPower();
+    }
 }
--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/src/Shooter.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/src/Shooter.java
@@ -1,4 +1,77 @@
 package org.firstinspires.ftc.teamcode.src;
 
+import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
+import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
+import com.qualcomm.robotcore.hardware.DcMotor;
+import com.qualcomm.robotcore.hardware.DcMotorSimple;
+import com.qualcomm.robotcore.hardware.Gamepad;
+import com.qualcomm.robotcore.hardware.HardwareMap;
+import org.firstinspires.ftc.robotcore.external.Telemetry;
+
 public class Shooter {
+    private final DcMotor shooterMotor;
+    //Shooters starting power constants
+    public double shooterPower = Constants.shooterPower;
+
+    public Shooter(HardwareMap hardwareMap, Telemetry telemetry) {
+        this.shooterMotor = hardwareMap.dcMotor.get(Constants.shooterMotor());
+        this.shooterMotor.setDirection(DcMotor.Direction.REVERSE);
+    }
+    //Shooter power increase function
+    public void increasePower() {
+        this.shooterPower = shooterPower + 0.1;
+        this.shooterPower = Math.min(shooterPower, 1.0);
+    }
+
+    //Shooter power decrease function
+    public void decreasePower() {
+        this.shooterPower = shooterPower - 0.1;
+        this.shooterPower = Math.max(shooterPower, 0);
+    }
+
+    //Constant for shooter motors stop
+    public void setZero(DcMotor motor) {
+        motor.setPower(0);
+    }
+
+    public void setMotorPower(double power) {
+        this.shooterMotor.setPower(power);
+    }
+
+    //Return shooter motors and powers
+    public DcMotor getShooterMotor() {
+        return this.shooterMotor;
+    }
+    public double getShooterPower() {
+        return this.shooterPower;
+    }
+
+    public void shooter100() {
+        this.shooterPower = 1;
+        setMotorPower(this.shooterPower);
+    }
+
+    public void shooter30() {
+        this.shooterPower = 0.3;
+        setMotorPower(this.shooterPower);
+    }
+
+    public void shooter50() {
+        this.shooterPower = 0.5;
+        setMotorPower(this.shooterPower);
+    }
+
+    public void shooter70() {
+        this.shooterPower = 0.7;
+        setMotorPower(this.shooterPower);
+    }
+
+    public void reverseMotor() {
+        this.shooterPower = this.shooterPower * -1;
+        setMotorPower(this.shooterPower);
+    }
+
+    public double shooterEncoderPosition() {
+        return this.shooterMotor.getCurrentPosition();
+    }
 }
