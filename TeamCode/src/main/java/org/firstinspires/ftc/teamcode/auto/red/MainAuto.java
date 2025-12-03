f644708744bd08eadafaf35145afd260a2c47e0f
Glowdisk <infernaldestinymc@gmail.com>
2025-12-02 22:19:39 -0800

    WIP

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/auto/red/MainAuto.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/auto/red/MainAuto.java
@@ -7,40 +7,53 @@ import com.acmerobotics.roadrunner.Vector2d;
 import com.acmerobotics.roadrunner.ftc.Actions;
 import com.qualcomm.robotcore.eventloop.opmode.Autonomous;
 import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
+import com.qualcomm.robotcore.hardware.DcMotor;
 
 
+import org.firstinspires.ftc.teamcode.Blocker;
 import org.firstinspires.ftc.teamcode.MecanumDrive;
+import org.firstinspires.ftc.teamcode.Shooter;
+import org.firstinspires.ftc.teamcode.commands.Intake;
+
 
 @Autonomous(name = "RedMainAuto", group = "Autonomous")
 public class MainAuto extends LinearOpMode {
+    private DcMotor intakeMotor;
+    Shooter shooter;
+    Blocker blocker;
+    Intake intake;
+
 
     double startPoseX = -56; //SmallTriangleAuto = 58
     double startPoseY = 45; //SmallTriangleAuto = 0
     double minYValue = 31;
     double maxYValue = 54;
 
+
     @Override
-    public void runOpMode(){
+    public void runOpMode() {
+        Intake intake = new Intake(hardwareMap);
+
         Pose2d startingPose = new Pose2d(startPoseX, startPoseY, Math.toRadians(127));
 
         MecanumDrive drive = new MecanumDrive(hardwareMap, startingPose);
 
         Action trajectoryAction = drive.actionBuilder(startingPose)
                 .waitSeconds(6) //Replace this with shoot code
-
-                .strafeToSplineHeading(new Vector2d(-20,minYValue), Math.toRadians(90))
+                .strafeToSplineHeading(new Vector2d(-20, minYValue), Math.toRadians(90))
                 //Start intake
-                .splineToConstantHeading(new Vector2d(-11,maxYValue),Math.toRadians(90))
+                // NEED AFTER
+                .splineToConstantHeading(new Vector2d(-11, maxYValue), Math.toRadians(90))
 
-                .strafeToSplineHeading(new Vector2d(startPoseX,startPoseY),Math.toRadians(127))
+                .strafeToSplineHeading(new Vector2d(startPoseX, startPoseY), Math.toRadians(127))
                 //stop intake
                 .waitSeconds(6) //Replace this with the shoot code
 
-                .strafeToSplineHeading(new Vector2d(3,minYValue), Math.toRadians(90))
+                .strafeToSplineHeading(new Vector2d(3, minYValue), Math.toRadians(90))
                 //Start intake
-                .splineToConstantHeading(new Vector2d(12,maxYValue),Math.toRadians(90))
+                .splineToConstantHeading(new Vector2d(12, maxYValue), Math.toRadians(90))
 
-                .strafeToSplineHeading(new Vector2d(startPoseX,startPoseY),Math.toRadians(127))
+                .strafeToSplineHeading(new Vector2d(startPoseX, startPoseY), Math.toRadians(127))
                 //stop intake
                 .waitSeconds(6) //Replace this shooter code
                 .build();
@@ -56,17 +69,31 @@ public class MainAuto extends LinearOpMode {
 
         Actions.runBlocking(
                 new SequentialAction(
-                                trajectoryAction
-                        )
+                        trajectoryAction
+                )
 //                        Example
 //                        trajectoryActionChosen,
 //                        lift.liftUp(),
 //                        claw.openClaw(),
 //                        lift.liftDown(),
 //                        trajectoryActionCloseOut
-                );
+        );
 
 
+    }
+
+    public Action intakeOnAction() {
+        return (telemetryPacket) -> {
+            intakeMotor.setPower(1.0);
+            return false; // false = finishes immediately
+        };
+    }
+
+    public Action intakeOffAction() {
+        return (telemetryPacket) -> {
+            intakeMotor.setPower(0);
+            return false;
+        };
     }
 
 }
