088969c14c6bcfc15d79a3b80eade5ce608e2200
Glowdisk <infernaldestinymc@gmail.com>
2025-12-03 19:21:10 -0800

    Emergency Auto for small triangle completed

---
+++ MeepMeepTesting/src/main/java/com/example/meepmeeptesting/blue/SmallTriangleEmergency.java
@@ -0,0 +1,37 @@
+package com.example.meepmeeptesting.blue;
+
+import com.acmerobotics.roadrunner.Pose2d;
+import com.noahbres.meepmeep.MeepMeep;
+import com.noahbres.meepmeep.roadrunner.DefaultBotBuilder;
+import com.noahbres.meepmeep.roadrunner.entity.RoadRunnerBotEntity;
+
+public class SmallTriangleEmergency {
+    public static void main(String[] args) {
+        MeepMeep meepMeep = new MeepMeep(800);
+
+        Pose2d startingPose = new Pose2d(62, -20, Math.toRadians(180));
+
+
+        RoadRunnerBotEntity robot = new DefaultBotBuilder(meepMeep)
+                // Set bot constraints: maxVel, maxAccel, maxAngVel, maxAngAccel, track width
+                .setConstraints(60, 60, Math.toRadians(90), Math.toRadians(90), 15)
+                .setStartPose(startingPose)
+                .build();
+
+        robot.runAction(
+                robot.getDrive()
+                        .actionBuilder(startingPose)
+                        .lineToX(40)
+                        .build()
+        );
+
+
+        meepMeep
+                .setBackground(MeepMeep.Background.FIELD_DECODE_JUICE_DARK)
+                .setDarkMode(true)
+                .setBackgroundAlpha(0.95f)
+                .addEntity(robot)
+                .start();
+
+    }
+}
